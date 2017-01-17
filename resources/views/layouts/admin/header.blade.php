eb36963d4c23f486169f9c9edbbaee1ac192e730
Thang <chukimthang94@gmail.com>
2017-01-17 09:39:14 +0700

    Layout admin page

---
+++ resources/views/layouts/admin/header.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">@lang('text.admin_toggle_navigation')</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">@lang('text.home_title')</a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">@lang('text.logout')</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/layouts/admin/menu.blade.php
@@ -0,0 +1,11 @@
+<div class="navbar-default sidebar" role="navigation">
+    <div class="sidebar-nav navbar-collapse">
+        <ul class="nav" id="side-menu">
+            <li>
+                <a href="#">
+                    @lang('text.menu_item_category')
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
