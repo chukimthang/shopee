55cb7bd1855eb81a79763d4d4a02071b44325521
chu kim thang <chu kim thang>
2017-04-18 09:05:50 -0700

    Fix convention js

--- public/seller/js/collection.js
+++ public/seller/js/collection.js
@@ -1,64 +1,66 @@
-var collection = function() {
-    this.init = function() {
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-            }
-        });
-        this.update();
-        this.delete();
-    }
+function collection() {
+  var current = this;
 
-    this.update = function() {
-        var dataId;
-        $('.update').on('click', function() {
-            dataId = $(this).data('id');
-            var dataName = $(this).parent().prev();
-            $('#collection-edit-name').val($.trim(dataName.html()));
-        });
+  this.init = function() {
+    $.ajaxSetup({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+      }
+    });
+    current.update();
+    current.delete();
+  }
 
-        $('#collection-edit').on('click', '.btn-primary', function() {
-            $.ajax({
-                url: '/seller/collection/updateAjax',
-                type: 'POST',
-                dataType: 'json',
-                data: {
-                    id: dataId,
-                    name: $('#collection-edit-name').val()
-                },
-                async: false,
-                success: function(data) {
-                    alert(data.sms);
-                    $('#collection-name-' + dataId).html($('#collection-edit-name').val());
-                    $('#collection-edit').modal('hide');
-                },
-                error: function(data) {
-                    var errors = '';
-                    for(datos in data.responseJSON){
-                        errors += data.responseJSON[datos] + '<br>';
-                    }
-                    $('.form-error').show().html(errors);
-                }
-            });
-        });
-    }
+  this.update = function() {
+    var dataId;
+    $('.update').on('click', function() {
+      dataId = $(this).data('id');
+      var dataName = $(this).parent().prev();
+      $('#collection-edit-name').val($.trim(dataName.html()));
+    });
 
-    this.delete = function() {
-        var dataId;
-        $('.delete').on('click', function() {
-            dataId = $(this).data('id');
-            $.ajax({
-                url: '/seller/collection/deleteAjax',
-                type: 'POST',
-                dataType: 'json',
-                data: {
-                    id: dataId
-                },
-                success: function(data) {
-                    alert(data.sms);
-                }
-            })
-            $(this).parent().parent().remove();
-        });
-    }
+    $('#collection-edit').on('click', '.btn-primary', function() {
+      $.ajax({
+        url: '/seller/collection/updateAjax',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+          id: dataId,
+          name: $('#collection-edit-name').val()
+        },
+        async: false,
+        success: function(data) {
+          alert(data.sms);
+          $('#collection-name-' + dataId).html($('#collection-edit-name').val());
+          $('#collection-edit').modal('hide');
+        },
+        error: function(data) {
+          var errors = '';
+          for(datos in data.responseJSON){
+            errors += data.responseJSON[datos] + '<br>';
+          }
+        $('.form-error').show().html(errors);
+        }
+      });
+    });
+  }
+
+  this.delete = function() {
+    var dataId;
+    $('.delete').on('click', function() {
+      dataId = $(this).data('id');
+      $.ajax({
+        url: '/seller/collection/deleteAjax',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+          id: dataId
+        },
+        success: function(data) {
+          alert(data.sms);
+        }
+      })
+      $(this).parent().parent().remove();
+    });
+  }
 }
